7dac28469cdfba645e74fc24b6e5bf03418e858c
rangesport01 <118473454+rangesport01@users.noreply.github.com>
2022-12-18 18:46:02 +0300

    Add files via upload

---
+++ laba 2/src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
